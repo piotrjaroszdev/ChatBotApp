47ce6095a1320d3a223e8fc4eab148c90a8641f6
LAPTOP\japit <piotr.jarosz.dev@gmail.com>
2025-06-25 01:40:32 +0200

    init DB added

--- ChatBotApp.Data/ChatContextFactory.cs
+++ ChatBotApp.Data/ChatContextFactory.cs
@@ -11,7 +11,6 @@ namespace ChatBotApp.Data
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true)
-                .AddJsonFile("appsettings.Development.json", optional: true)
                 .Build();
 
             var connectionString = configuration.GetConnectionString("DefaultConnection");
