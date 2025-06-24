686c03d34095d9e36c49cff7092cc55224b065f3
LAPTOP\japit <piotr.jarosz.dev@gmail.com>
2025-06-25 00:52:26 +0200

    DefaultConnection has been added

--- ChatBotApp.Data/ChatContextFactory.cs
+++ ChatBotApp.Data/ChatContextFactory.cs
@@ -1,15 +1,20 @@
 ﻿using ChatBotApp.Data.ChatBotApp.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-
+using Microsoft.Extensions.Configuration;
 namespace ChatBotApp.Data
 {
     public class ChatContextFactory : IDesignTimeDbContextFactory<ChatContext>
     {
         public ChatContext CreateDbContext(string[] args)
         {
+            var basePath = Directory.GetCurrentDirectory();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
             var optionsBuilder = new DbContextOptionsBuilder<ChatContext>();
-            optionsBuilder.UseSqlServer("Server=localhost;Database=ChatBotDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("DefaultConnection");
 
             return new ChatContext(optionsBuilder.Options);
         }
